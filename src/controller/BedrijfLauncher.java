70647940ffea4d78c8c8e7aca38c691925400307
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-10-03 09:29:31 +0200

    Opdracht 2.1

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -9,18 +9,30 @@ import model.Persoon;
 public class BedrijfLauncher {
 
     public static void main(String[] args) {
-        System.out.println(Persoon.aantalPersonen);
+        System.out.println(Persoon.getAantalPersonen());
         Persoon baas = new Persoon("Mark", "Den Haag", 10000);
-        System.out.println(Persoon.aantalPersonen);
-        System.out.println(baas.personeelsNummer);
+        System.out.println(Persoon.getAantalPersonen());
+        System.out.println(baas.getPersoneelsNummer());
         Persoon medewerker = new Persoon("Caroline", "Delft", 4000);
-        System.out.println(Persoon.aantalPersonen);
-        System.out.println(medewerker.personeelsNummer);
+        System.out.println(Persoon.getAantalPersonen());
+        System.out.println(medewerker.getPersoneelsNummer());
 
         Persoon assistent = new Persoon("Klaas");
         Persoon manager = new Persoon();
-        System.out.println(Persoon.aantalPersonen);
-        System.out.printf("%s verdient %.2f per jaar\n", baas.naam, baas.berekenJaarinkomen());
-        System.out.printf("%s woont in %s\n", assistent.naam, assistent.woonplaats);
+        System.out.println(Persoon.getAantalPersonen());
+
+        String bonus = "geen";
+        if (baas.heeftRechtOpBonus()) {
+            bonus = "wel";
+        }
+        System.out.printf("%s verdient %.2f en heeft %s recht op een bonus\n",
+                baas.getNaam(), baas.getMaandSalaris(), bonus);
+
+        bonus = "geen";
+        if (medewerker.heeftRechtOpBonus()) {
+            bonus = "wel";
+        }
+        System.out.printf("%s verdient %.2f en heeft %s recht op een bonus\n",
+                medewerker.getNaam(), medewerker.getMaandSalaris(), bonus);
     }
 }
--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -9,37 +9,63 @@ public class Persoon {
     private static final String DEFAULT_WOONPLAATS = "Onbekend";
     private static final double DEFAULT_MAAND_SALARIS = 0.0;
 
+    private static final double GRENSWAARDE_BONUS = 4500.0;
+
     private static final int MAANDEN_PER_JAAR = 12;
 
-    public static int aantalPersonen = 0;
+    private static int aantalPersonen = 0;
 
-    public int personeelsNummer;
-    public String naam;
-    public String woonplaats;
-    public double maandSalaris;
+    private int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandSalaris;
 
     public Persoon(String naam, String woonplaats, double maandSalaris) {
         this.naam = naam;
         this.woonplaats = woonplaats;
-        this.maandSalaris = maandSalaris;
+        setMaandSalaris(maandSalaris);
+
         this.personeelsNummer = ++aantalPersonen;
     }
 
     public Persoon(String naam) {
-        this.naam = naam;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandSalaris = DEFAULT_MAAND_SALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(naam, DEFAULT_WOONPLAATS, DEFAULT_MAAND_SALARIS);
     }
 
     public Persoon() {
-        this.naam = DEFAULT_NAAM;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandSalaris = DEFAULT_MAAND_SALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(DEFAULT_NAAM);
     }
 
     public double berekenJaarinkomen() {
         return MAANDEN_PER_JAAR * maandSalaris;
+    }
+
+    public boolean heeftRechtOpBonus() {
+        return maandSalaris >= GRENSWAARDE_BONUS;
+    }
+
+    public static int getAantalPersonen() {
+        return aantalPersonen;
+    }
+
+    public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getMaandSalaris() {
+        return maandSalaris;
+    }
+
+    public void setMaandSalaris(double maandSalaris) {
+        if (maandSalaris < 0) {
+            System.err.printf("Een maandsalaris van %d is niet geldig, het maandsalaris wordt op %d gezet.\n",
+                    maandSalaris, DEFAULT_MAAND_SALARIS);
+            maandSalaris = DEFAULT_MAAND_SALARIS;
+        }
+        this.maandSalaris = maandSalaris;
     }
 }
